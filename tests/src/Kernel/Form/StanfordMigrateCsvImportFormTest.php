fc44e702551fcbfededcccb9b37cc9c1706ba634
Mike Decker <pookmish@gmail.com>
2021-04-05 17:19:57 -0700

    more form tests

--- tests/src/Kernel/Form/StanfordMigrateCsvImportFormTest.php
+++ tests/src/Kernel/Form/StanfordMigrateCsvImportFormTest.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\Tests\stanford_migrate\Kernel\Form;
 
+use Drupal\Core\Form\FormState;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\migrate_plus\Entity\Migration;
+use Drupal\migrate_plus\Entity\MigrationGroup;
 use Drupal\migrate_plus\Entity\MigrationInterface;
 use Drupal\Tests\stanford_migrate\Kernel\StanfordMigrateKernelTestBase;
 use Symfony\Component\HttpFoundation\Request;
@@ -56,13 +58,7 @@ class StanfordMigrateCsvImportFormTest extends StanfordMigrateKernelTestBase {
    * CSV Importers have permission access.
    */
   public function testCsvPermissionAccess() {
-    $migration = Migration::load('stanford_migrate');
-    $source = $migration->get('source');
-    $source['plugin'] = 'csv';
-    $source['path'] = '/tmp/tmp.csv';
-    $source['ids'] = ['foo'];
-    $migration->set('source', $source)->save();
-    $this->setMigrationRequest($migration);
+    $this->setCsvMigrationRequest();
 
     $account = $this->createMock(AccountInterface::class);
     $form_object = \Drupal::entityTypeManager()
@@ -73,13 +69,46 @@ class StanfordMigrateCsvImportFormTest extends StanfordMigrateKernelTestBase {
     $this->assertTrue($form_object->access($account)->isAllowed());
   }
 
+  public function testBuildForm() {
+    $this->setCsvMigrationRequest();
+    $form = [];
+    $form_state = new FormState();
+    $form_object = \Drupal::entityTypeManager()
+      ->getFormObject('migration', 'csv-upload');
+    $form_object->setEntity(Migration::load('stanford_migrate'));
+
+    $form = $form_object->buildForm($form, $form_state);
+    $this->assertArrayNotHasKey('forget', $form);
+
+    \Drupal::state()->set('stanford_migrate.csv.stanford_migrate', [1, 2, 3]);
+    $form = $form_object->buildForm($form, $form_state);
+    $this->assertArrayHasKey('forget', $form);
+  }
+
+  /**
+   * Modify the migration entity and set it on the current request.
+   */
+  protected function setCsvMigrationRequest() {
+    $migration = Migration::load('stanford_migrate');
+    $source = $migration->get('source');
+    $source['plugin'] = 'csv';
+    $source['path'] = '/tmp/tmp.csv';
+    $source['ids'] = ['foo'];
+    $migration->set('source', $source)->save();
+    $this->setMigrationRequest($migration);
+  }
+
   /**
    * Set the current request on the request stack to have a migration entity.
    *
    * @param \Drupal\migrate_plus\Entity\MigrationInterface $migration
    */
   protected function setMigrationRequest(MigrationInterface $migration) {
-    $attributes = ['migration' => $migration];
+
+    $attributes = [
+      'migration_group' => MigrationGroup::load('stanford_migrate'),
+      'migration' => $migration,
+    ];
     $request = new Request([], [], $attributes);
     \Drupal::requestStack()->push($request);
   }
