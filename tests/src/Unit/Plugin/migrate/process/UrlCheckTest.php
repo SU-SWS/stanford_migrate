06a39330fedaca0a592b4316b1741ba1bee1bce1
pookmish <pookmish@gmail.com>
2025-10-02 13:02:05 -0700

    URL Check migrate process should check for absolute urls (#115)

--- tests/src/Unit/Plugin/migrate/process/UrlCheckTest.php
+++ tests/src/Unit/Plugin/migrate/process/UrlCheckTest.php
@@ -7,6 +7,7 @@ use Drupal\migrate\MigrateSkipRowException;
 use Drupal\migrate\Row;
 use Drupal\stanford_migrate\Plugin\migrate\process\UrlCheck;
 use Drupal\Tests\UnitTestCase;
+use PHPUnit\Framework\Attributes\TestWith;
 
 /**
  * Class UrlCheckTest.
@@ -17,31 +18,45 @@ class UrlCheckTest extends UnitTestCase {
   /**
    * Skipping process throws an exception.
    */
-  public function testProcess() {
+  #[TestWith(['url' => 'https://google.com', 'skipped' => FALSE])]
+  #[TestWith(['url' => ' https://google.com', 'skipped' => TRUE])]
+  #[TestWith(['url' => 'Foo Bar', 'skipped' => TRUE])]
+  #[TestWith(['url' => 'www.google.com', 'skipped' => TRUE])]
+  public function testProcess(string $url, bool $skipped) {
     $plugin = new UrlCheck(['method' => 'process'], '', []);
     $migrate = $this->createMock(MigrateExecutableInterface::class);
     $row = $this->createMock(Row::class);
 
-    $value = $plugin->transform('https://google.com', $migrate, $row, NULL);
-    $this->assertEquals('https://google.com', $value);
+    $value = $plugin->transform($url, $migrate, $row, NULL);
 
-    $plugin->transform('Foo Bar', $migrate, $row, NULL);
-    $this->assertTrue($plugin->isPipelineStopped());
+    if ($skipped) {
+      $this->assertTrue($plugin->isPipelineStopped());
+    }
+    else {
+      $this->assertEquals($url, $value);
+    }
   }
 
   /**
    * Skipping a row throws an exception.
    */
-  public function testRow() {
+  #[TestWith(['url' => 'https://google.com', 'skipped' => FALSE])]
+  #[TestWith(['url' => ' https://google.com', 'skipped' => TRUE])]
+  #[TestWith(['url' => 'Foo Bar', 'skipped' => TRUE])]
+  #[TestWith(['url' => 'www.google.com', 'skipped' => TRUE])]
+  public function testRow(string $url, bool $skipped) {
     $plugin = new UrlCheck(['method' => 'row'], '', []);
     $migrate = $this->createMock(MigrateExecutableInterface::class);
     $row = $this->createMock(Row::class);
 
-    $value = $plugin->transform('https://google.com', $migrate, $row, NULL);
-    $this->assertEquals('https://google.com', $value);
-
-    $this->expectException(MigrateSkipRowException::class);
-    $plugin->transform('Foo Bar', $migrate, $row, NULL);
+    if ($skipped) {
+      $this->expectException(MigrateSkipRowException::class);
+      $plugin->transform($url, $migrate, $row, NULL);
+    }
+    else {
+      $value = $plugin->transform($url, $migrate, $row, NULL);
+      $this->assertEquals($url, $value);
+    }
   }
 
 }
